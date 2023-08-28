db07d3439586a72afed6f260d1965e6265a848b9
sandeep0077 <sm671903@gmail.com>
2023-08-28 18:20:40 +0530

    Initial commit, created model classes

---
+++ order-service/src/main/java/com/programmingtechie/orderservice/model/Order.java
@@ -0,0 +1,25 @@
+package com.programmingtechie.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String orderNumber;
+    private List<OrderLineItems> orrderLineItemList;
+
+}
